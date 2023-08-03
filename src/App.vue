8de1609f660ace40d95b433048f0c93f5ba112b9
Turbosaa <laixingyu_a@163.com>
2023-08-03 16:59:34 +0800

    xxx

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
     <button @click="isShowDemo = !isShowDemo">{{ isShowDemo ? '隐藏' : '显示' }}</button>
     <Demo v-if="isShowDemo" />
-    <hr>
-    <Test />
 </template>
 
 <script>
 import { ref } from 'vue'
 import Demo from './components/Demo'
-import Test from './components/Test'
 export default {
     name: 'App',
-    components: { Demo, Test },
+    components: { Demo },
     setup() {
         let isShowDemo = ref(true)
         return { isShowDemo }
