5bcd880d42e30973ec642c80efc5a5e2146093ed
Turbosaa <laixingyu_a@163.com>
2023-08-09 17:28:20 +0800

    XXX

---
+++ 18_src_provide与inject/components/Son.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="son">
+        <h3>我是Son组件（孙），{{ car.name }}--{{ car.price }}W</h3>
+        <button @click="car.price++">修改price</button>
+    </div>
+</template>
+
+<script>
+import { inject } from 'vue'
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Son',
+    setup() {
+        let car = inject('car')
+        return { car }
+    }
+}
+</script>
+
+<style>
+.son {
+    background-color: orange;
+    padding: 10px;
+}
+</style>
